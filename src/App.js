89d43533480162f125350cce2fe12e7619374b4f
tigger <tigger@cherground.com>
2023-10-27 13:56:14 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { 
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom"
+import Home from "./routes/Home"
+import Detail from "./routes/Detail"
+
+function ToDos() {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value)
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray])
+        setToDo("");
+    };
+    return ( 
+        <div>
+            <h1> My To Dos - ({ toDos.length }) </h1>  
+            <form onSubmit={ onSubmit }>
+            <input 
+                type = "text"
+                placeholder = "Write your to do...."
+                value = { toDo }
+                onChange = { onChange }
+            /> 
+            <button>Add To Do</button> 
+            </form> 
+            <hr /> 
+            { toDos.map((item, index) => (
+                <li key={index}>{ item }</li>
+            )) }  
+        </div>
+    );
+}
+
+function Coins() {
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        const url = 'https://api.coinpaprika.com/v1/tickers';
+        fetch(url)
+            .then(response => response.json())
+            .then((json) => {
+                setCoins(json)
+                setLoading(false)
+            });
+    }, []);
+    return (
+        <div>
+            <h1>The Coins! { loading ? '' : `- (${coins.length})` }</h1>
+            { loading 
+                ? (
+                    <strong>Loading...</strong>
+                ) 
+                : (
+                    <select>
+                        { coins.map((coin) => (
+                            <option key={coin.id}>
+                                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+                            </option>
+                        )) }
+                    </select>
+                )
+            }
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/hello">
+                    <h1>hello</h1>
+                </Route>
+                <Route path="/movie/:id">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie"
+
+function Detail() {
+    const { id } = useParams();
+    const [ movie, setMovie ] = useState(null);
+    const getMovice = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovie(json.data.movie);
+    };
+    useEffect(() => {
+        getMovice();
+    }, []);
+    return (
+        <div>
+            <h1>Detail</h1>
+            { movie != null 
+                ? <Movie 
+                    id={movie.id} 
+                    coverImg={movie.medium_cover_image}
+                    title={movie.title}
+                    summary={movie.summary}
+                    genres={movie.genres}
+                /> 
+                : null 
+            }
+        </div>
+    );
+}
+
+export default Detail;
