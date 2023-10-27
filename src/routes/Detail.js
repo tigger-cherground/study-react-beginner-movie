3be726234c28e9b34f6875cc6f7150b39b617e02
tigger <tigger@cherground.com>
2023-10-27 14:09:12 +0900

    add styles

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -22,6 +22,7 @@ function Detail() {
                     id={movie.id} 
                     coverImg={movie.medium_cover_image}
                     title={movie.title}
+                    year={movie.year}
                     summary={movie.summary}
                     genres={movie.genres}
                 /> 
